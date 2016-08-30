d1e9e096201249924fb5579f7527d9633891b0d3
yoarmaniii <armanijolee@gmail.com>
2016-08-30 13:23:32 -0400

    Complete Work

---
+++ Documents/workspace/reverse-reverse/src/Public.java
@@ -0,0 +1,17 @@
+
+public class Public {
+	
+
+	public static void main (String[] args) {
+ int myNumber = 100;
+ short myShort = 847;
+ long myLong = 9797;
+ 
+		 
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myNumber + myShort);
+		System.out.println(myNumber + myShort - myNumber);
+	}
+}
